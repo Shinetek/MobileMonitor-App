368926f0b2af036e28cf5afccd49c0c8b408b07b
FanTaSyLin <fanlin19830908@hotmail.com>
2017-04-20 17:07:53 +0800

    1

--- www/js/lv1fastview/controller.js
+++ www/js/lv1fastview/controller.js
@@ -2,13 +2,30 @@
 
     "use strict";
 
-    angular.module("starter")
+    angular.module("starter.controllers")
         .controller("L1FastViewController", L1FastViewCtrlFn);
     
     L1FastViewCtrlFn.$inject = ["L1FastViewServices", "$scope"];
     
     function L1FastViewCtrlFn(L1FastViewServices, $scope) {
         var self = this;
+        // 当前navTab
+        self.instNavCurrentItem = "sdy";
+
+        // 判断页签是否为选中状态
+        self.instNavItemIsSelected = _instNavItemIsSelected;
+        // 选择页签
+        self.selectInstNavItem = _selectInstNavItem;
+
+        function _instNavItemIsSelected(navName) {
+            return self.instNavCurrentItem === navName;
+        }
+
+        function _selectInstNavItem(navName) {
+            alert("1");
+            self.instNavCurrentItem = navName;
+        }
+
     }
 
 })();
