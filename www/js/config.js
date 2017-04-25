247beb4166b0ca27636fa43e62f630cd4ee8e76a
FanTaSyLin <fanlin19830908@hotmail.com>
2017-04-25 15:45:07 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	.gitignore
    #	www/index.html
    #	www/partials/lv1fastview/lv1fastview.html

--- www/js/config.js
+++ www/js/config.js
@@ -45,7 +45,7 @@ angular.module('starter')
     .state("tab.lv1fastview", {
       url: "/lv1fastview",
       views: {
-        "tab-lv1fastview": {
+	      'tab-lv1fastview': {
             templateUrl: "partials/lv1fastview/lv1fastview.html",
             controller:'L1FastViewController'
         }
@@ -61,36 +61,36 @@ angular.module('starter')
       }
     })
 
-    /*.state('tab.subsystem-custom',{
+      /*.state('tab.subsystem-custom',{
       url:'/subsystem/:state',
       views:{
        'tab-subsystem':{
           templateUrl:'partials/subsystem/subsystem-custom.html',
-         controller:'SubSystemCustomCtrl'
+       controller:'SubSystemCustomCtrl'
         }
       }
-    })*/
+       })*/
 
-    //添加成像仪观测任务运行状态监测
-    .state('tab.apparatus-cxy',{
-      url:'/subsystem/:listname',
-      views:{
-        'tab-subsystem':{
-          templateUrl:'partials/apparatus/apparatus-list.html',
-          controller:"ApparatusListCtrl"
-        }
-      }
-    })
+	  //添加成像仪观测任务运行状态监测
+	  .state('tab.apparatus-cxy', {
+		  url: '/subsystem/:listname',
+		  views: {
+			  'tab-subsystem': {
+				  templateUrl: 'partials/apparatus/apparatus-list.html',
+				  controller: 'ApparatusListCtrl',
+			  },
+		  },
+	  })
 
-    //增加仪器下当前任务详细报告
-    .state("tab.apparatus-task",{
-      url:"/subsystem/:listname/:taskid",
-      views:{
-        "tab-subsystem":{
-          templateUrl:'partials/apparatus/apparatus-task.html',
-          controller:"ApparatusTaskCtrl"
-        }
-      }
+	  //增加仪器下当前任务详细报告
+	  .state('tab.apparatus-task', {
+		  url: '/subsystem/:listname/:taskid',
+		  views: {
+			  'tab-subsystem': {
+				  templateUrl: 'partials/apparatus/apparatus-task.html',
+				  controller: 'ApparatusTaskCtrl',
+			  },
+		  },
     });
 
   // if none of the above states are matched, use this as the fallback
