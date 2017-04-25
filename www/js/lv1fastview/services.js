e18a931bd3e2b3b60c082898ab0a003c91a8a4cc
FanTaSyLin <fanlin19830908@hotmail.com>
2017-04-25 16:12:55 +0800

    Merge branch 'develop' of https://github.com/Shinetek/MobileMonitor-App into develop
    # Conflicts:
    #	www/js/lv1fastview/controller.js

--- www/js/lv1fastview/services.js
+++ www/js/lv1fastview/services.js
@@ -8,7 +8,13 @@
     L1FastViewServicesFn.$inject = ["$http"];
 
     function L1FastViewServicesFn($http) {
-        return {};
+        return {
+	        getInistTaskList: _getInistTaskList
+        };
+
+        function _getInistTaskList(url, successFn, errorFn) {
+            $http.get(url).success(successFn).error(errorFn);
+        }
     }
 
 })();
