b86e70c11fc378f7f20538e888862aa53c332eac
selenityboy <liuhequn1123@sina.com>
2017-04-28 16:45:08 +0800

    Merge branch 'develop' of https://github.com/Shinetek/MobileMonitor-App into develop
    # Conflicts:
    #	www/js/lv1fastview/controller.js
    #	www/partials/lv1fastview/lv1fastview.html

--- www/js/FaultView/FaultViewCtrl.js
+++ www/js/FaultView/FaultViewCtrl.js
@@ -2,89 +2,116 @@
  * Created by qq on 2017/4/25.
  */
 angular
-    .module('starter.controllers')
-    .controller('FaultViewCtrl',FaultViewCtrl)
+  .module('starter.controllers')
+  .controller('FaultViewCtrl', FaultViewCtrl)
 
-FaultViewCtrl.$inject = ['$scope', 'SQLiteService', 'HttpService', 'Systems', '$ionicPopover'];
+FaultViewCtrl.$inject = ['$scope', 'SQLiteService', 'HttpService', 'Systems', '$ionicPopover', '$ionicLoading'];
 
-function FaultViewCtrl($scope, SQLiteService, HttpService, Systems, $ionicPopover){
+function FaultViewCtrl($scope, SQLiteService, HttpService, Systems, $ionicPopover, $ionicLoading) {
 
-    console.log("FaultViewCtrl");
-    var date = new Date();
-    $scope.isShow = true;
+  //console.log("FaultViewCtrl");
+  var date = new Date();
+  $scope.isShow = true;
 
-    $scope.popover = $ionicPopover.fromTemplateUrl('my-popover.html', {
-        scope: $scope
+  $scope.data = {};
+  $scope.data.currdate = date;
+  $scope.data.queryDescribe = "";
+  $scope.data.queryType = "ALL";
+
+
+  $scope.popover = $ionicPopover.fromTemplateUrl('my-popover.html', {
+    scope: $scope
+  });
+
+  // .fromTemplateUrl() 方法
+  $ionicPopover.fromTemplateUrl('my-popover.html', {
+    scope: $scope
+  }).then(function (popover) {
+    $scope.popover = popover;
+  });
+
+  $scope.openPopover = function ($event) {
+    $scope.popover.show($event);
+  };
+
+  $scope.closePopover = function () {
+    $scope.popover.hide();
+  };
+  // 清除浮动框
+  $scope.$on('$destroy', function () {
+    $scope.popover.remove();
+  });
+  // 在隐藏浮动框后执行
+  $scope.$on('popover.hidden', function () {
+    // 执行代码
+  });
+  // 移除浮动框后执行
+  $scope.$on('popover.removed', function () {
+    // 执行代码
+  });
+
+  var ss = new Array();
+
+  updateFaultData();
+
+  function updateFaultData() {
+
+    $ionicLoading.show({
+      content: 'Loading',
+      animation: 'fade-in',
+      showBackdrop: true,
+      maxWidth: 200,
+      showDelay: 0
     });
 
-    // .fromTemplateUrl() 方法
-    $ionicPopover.fromTemplateUrl('my-popover.html', {
-        scope: $scope
-    }).then(function(popover) {
-        $scope.popover =  popover;
-    });
+    // 获取当前显示的故障数据
+    HttpService.getdata("guzhang.json").then(function (res) {
+      for (var i = 0; i < res.length; i++) {
+        ss[i] = res[i];
+        ss[i].happen_dt = updateDateTime(res[i].happen_dt);
+        ss[i].status = (res[i].status);
+      }
 
-    console.log("fromTemplateUrl" + $scope.popover == null);
-
-    $scope.openPopover = function($event) {
-        $scope.popover.show($event);
-    };
-
-    $scope.closePopover = function() {
-        $scope.popover.hide();
-    };
-    // 清除浮动框
-    $scope.$on('$destroy', function() {
-        $scope.popover.remove();
-    });
-    // 在隐藏浮动框后执行
-    $scope.$on('popover.hidden', function() {
-        // 执行代码
-    });
-    // 移除浮动框后执行
-    $scope.$on('popover.removed', function() {
-        // 执行代码
-    });
-    //document.getElementById("datePicker").value = date.toISOString().substr(0, 10);
-
-    var s
-    var ss = new Array();
-    HttpService.getdata("guzhang.json").then(function(res){
-        for(var i = 0;i<res.length;i++){
-            s = new Object();
-            s.code = res[i].code;
-            s.describe = res[i].describe;
-            s.happen_dt = updateDateTime(res[i].happen_dt);
-            s.status = (res[i].status);
-
-            ss[i] = s;
-        }
+      $ionicLoading.hide();
+    }, function(err){
+      $ionicLoading.hide();
     })
+  }
 
+  $scope.filterData = function () {
 
-    function updateDateTime(datetime){
+    console.log("$scope.data.currdate : " + $scope.data.currdate.toISOString().substr(0, 10));
+    console.log("$scope.data.queryDescribe : " + $scope.data.queryDescribe);
+    console.log("$scope.data.queryType : " + $scope.data.queryType);
 
-        var datestring = "";
-        datestring +=datetime.substr(0,4) + "-";
-        datestring +=datetime.substr(4,2) + "-";
-        datestring +=datetime.substr(6,2) + " ";
-        datestring +=datetime.substr(8,2) + ":";
-        datestring +=datetime.substr(10,2) + ":";
-        datestring +=datetime.substr(12,2);
+  }
 
-        return datestring;
+  // 把时间字符串更新为标准型-YYYY-MM-DD hh
+  function updateDateTime(datetime) {
+
+    var datestring = "";
+    datestring += datetime.substr(0, 4) + "-";
+    datestring += datetime.substr(4, 2) + "-";
+    datestring += datetime.substr(6, 2) + " ";
+    datestring += datetime.substr(8, 2) + ":";
+    datestring += datetime.substr(10, 2) + ":";
+    datestring += datetime.substr(12, 2);
+
+    return datestring;
+  }
+
+  function updateStatus(status) {
+    var text;
+    switch (status) {
+      case "undeal":
+        text = "未处理";
+        break;
+      default :
+        text = "已处理";
+        break;
     }
+    return text;
+  }
 
-    function updateStatus(status){
-        var text ;
-        switch (status){
-            case "undeal":text = "未处理";
-                break;
-            default :text = "已处理";
-                break;
-        }
-        return text;
-    }
-
-    $scope.faults = ss;
+  $scope.faults = ss;
 }
--- www/js/services.js
+++ www/js/services.js
@@ -1,91 +1,98 @@
 angular.module('starter.services', ['ngCordova'])
 
-  .factory('Sensors',function(){
+  .factory('ShareData',function(){
+    var describe = "";
+    return {
+
+    }
+  })
+
+  .factory('Sensors', function () {
     var sensors = [{
-      id:"cxy",
-      name:'成像仪',
-      icon:'img/accessories-camera-icon.png',
-      success:'10',
-      fail:'10'
-    },{
-      id:"tcy",
-      name:'探测仪',
-      icon:'img/midori-globe-icon.png',
-      success:'10',
-      fail:'10'
-    },{
-      id:"sdy",
-      name:'闪电仪',
-      icon:'img/flash.png',
-      success:'10',
-      fail:'10'
+      id: "cxy",
+      name: '成像仪',
+      icon: 'img/accessories-camera-icon.png',
+      success: '10',
+      fail: '10'
+    }, {
+      id: "tcy",
+      name: '探测仪',
+      icon: 'img/midori-globe-icon.png',
+      success: '10',
+      fail: '10'
+    }, {
+      id: "sdy",
+      name: '闪电仪',
+      icon: 'img/flash.png',
+      success: '10',
+      fail: '10'
     }];
 
     return {
-      all: function() {
+      all: function () {
         return sensors;
       },
-      getSensor:function(name){
-          for (var i = 0; i < sensors.length; i++) {
-            if (sensors[i].name === name) {
-              return sensors[i];
-            }
+      getSensor: function (name) {
+        for (var i = 0; i < sensors.length; i++) {
+          if (sensors[i].name === name) {
+            return sensors[i];
           }
-          return null;
+        }
+        return null;
       }
     };
   })
 
-  .factory('Systems',function(){
+  .factory('Systems', function () {
     var systems = [{
-      id:'DTS',
-      name:'数据获取',
-      icon:'img/data-download.png',
+      id: 'DTS',
+      name: '数据获取',
+      icon: 'img/data-download.png',
       selected: false
-    },{
-      id:'NRS',
-      name:'图像定位',
-      icon:'img/gps.png',
+    }, {
+      id: 'NRS',
+      name: '图像定位',
+      icon: 'img/gps.png',
       selected: false
-    },{
-      id:'PGS',
-      name:'产品生成',
-      icon:'img/social_round_product_hunt_149px_1196573_easyicon.net.png',
+    }, {
+      id: 'PGS',
+      name: '产品生成',
+      icon: 'img/social_round_product_hunt_149px_1196573_easyicon.net.png',
       selected: false
-    },{
-      id:'SWS',
-      name:'空间天气',
-      icon:'img/weather.png',
+    }, {
+      id: 'SWS',
+      name: '空间天气',
+      icon: 'img/weather.png',
       selected: false
-    },{
-      id:'MCS',
-      name:'任务管控',
-      icon:'img/task.png',
+    }, {
+      id: 'MCS',
+      name: '任务管控',
+      icon: 'img/task.png',
       selected: false
-    },{
-      id:'DSS',
-      name:'数据服务',
-      icon:'img/database(1).png',
+    }, {
+      id: 'DSS',
+      name: '数据服务',
+      icon: 'img/database(1).png',
       selected: false
-    },{
-      id:'MRS',
-      name:'应用示范',
-      icon:'img/Application_213px_1194722_easyicon.net.png',
+    }, {
+      id: 'MRS',
+      name: '应用示范',
+      icon: 'img/Application_213px_1194722_easyicon.net.png',
       selected: false
-    },{
-      id:'CVS',
-      name:'定标检验',
-      icon:'img/check_128px_1180491_easyicon.net.png',
+    }, {
+      id: 'CVS',
+      name: '定标检验',
+      icon: 'img/check_128px_1180491_easyicon.net.png',
       selected: false
-    },{
-      id:'CNS',
-      name:'计算机网络',
-      icon:'img/network.png',
+    }, {
+      id: 'CNS',
+      name: '计算机网络',
+      icon: 'img/network.png',
       selected: false
     }];
 
     return {
-      all: function() {
+      all: function () {
         return systems;
       }
     };
